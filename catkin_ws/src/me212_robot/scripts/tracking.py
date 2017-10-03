f9e539bff690f5d4ad58ffdffa757338cdd7fc9c
kuokuo <c722996@gmail.com>
2017-10-03 14:11:54 +0800

    test

--- catkin_ws/src/me212_robot/scripts/tracking.py
+++ catkin_ws/src/me212_robot/scripts/tracking.py
@@ -31,37 +31,37 @@ class Tracking:
 		#         3) straight line again.
 
 		# stages 1
-		if self.state == 1
-			if (0 < x) and (x < 1)
+		if self.state == 1:
+			if (0 <= x) and (x < 1):
 				self.leftMotor.run(1)
 				self.rightMotor.run(1)
 			else:
 				self.state = 2
 
 				# one method
-				self.leftMotor.run(2)
+				#self.leftMotor.run(2)
 				# self.rightMotor.run(1)
 
 				# another method
-				# self.leftMotor.setSpeed(self.left_pwm)		# 60
-				# self.rightMotor.setSpeed(self.left_pwm * 2)	# 120
+				self.leftMotor.setSpeed(self.left_pwm)		# 60
+				self.rightMotor.setSpeed(self.left_pwm * 2)	# 120
 
 		# stages 2
-		if self.state == 2
-			if (theta >= pi)
+		if self.state == 2:
+			if (theta >= pi):
 				self.state = 3
 
 				# one method
-				self.leftMotor.run(1)
+				#self.leftMotor.run(1)
 				# self.rightMotor.run(1)
 
 				# another method
-				# self.leftMotor.setSpeed(self.left_pwm)
-				# self.rightMotor.setSpeed(self.right_pwm)
+				self.leftMotor.setSpeed(self.left_pwm)
+				self.rightMotor.setSpeed(self.right_pwm)
 
 		# stages 3
-		if self.state == 3
-			if x <= 0		# stop
+		if self.state == 3:
+			if x <= 0:		# stop
 				# self.state = 
 				self.leftMotor.run(4)
 				self.rightMotor.run(4)
